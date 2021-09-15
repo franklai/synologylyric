5cf57d7237c44a60df283f6338ea0a168f0c7198
Frank Lai <franklai@cmlab.csie.ntu.edu.tw>
2021-09-15 21:59:10 +0800

    add no lyrics test for kkbox

--- tests/FujirouKkboxTest.php
+++ tests/FujirouKkboxTest.php
@@ -13,8 +13,8 @@ final class FujirouKkboxTest extends LyricsTestCase
         $title = '浪流連';
         $answer = array(
             'artist' => '茄子蛋',
-            'title'  => '浪流連',
-            'id'     => 'https://www.kkbox.com/tw/tc/song/22y00C6.d.JIIzpXIIzpX0XL-index.html'
+            'title' => '浪流連',
+            'id' => 'https://www.kkbox.com/tw/tc/song/22y00C6.d.JIIzpXIIzpX0XL-index.html',
         );
 
         $this->search($artist, $title, $answer);
@@ -24,6 +24,14 @@ final class FujirouKkboxTest extends LyricsTestCase
     {
         $id = 'https://www.kkbox.com/tw/tc/song/22y00C6.d.JIIzpXIIzpX0XL-index.html';
         $path = 'FujirouKkbox.egg_plantegg.txt';
+
+        $this->get($id, $path);
+    }
+
+    public function testNoLyrics()
+    {
+        $id = 'https://www.kkbox.com/tw/tc/song/vZP00L48426WRyKqWRyKq0XL-index.html';
+        $path = $path = 'FujirouKkbox.no_lyrics.txt';
 
         $this->get($id, $path);
     }
